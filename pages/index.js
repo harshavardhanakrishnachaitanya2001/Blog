e8ba550de0cf24e11b0150290efdf77c3b1a4f6e
Harshavardhan <187y1a0575@gmail.com>
2020-08-19 23:14:49 +0530

    FINALLY COMPLETED WITH ABOUT SECTION OF THE HOME PAGE

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,7 @@ export default function Home() {
         <p className="about">
           I am G.H.Krishna Chaitanya, pursuing my B.tech IIIrd year in MarriLaxman Reddy instute of technology and management. I am an enthusiastic 
           front-end web developer, working primarily with React along with HTML, CSS, Bootstrap, React-native, context, git and Nextjs. I also enjoy working with 
-           java, C language and python programming languages
+           java, C and python programming languages
         </p>
       </main>
     </div>
