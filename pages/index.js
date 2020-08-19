1e19efde67bc83da8e38c11732fbfe0da7ae745d
Harshavardhan <187y1a0575@gmail.com>
2020-08-19 19:58:09 +0530

    Second commit

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
+    <style>
+      {`
+      button{
+        margin-left:10px;
+      }
+      `}
+    </style>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -16,10 +23,10 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-        Tasks done:<br />
+        Projects done:<br />
         <Link href="https://todoapplication-nu.vercel.app/"><button>Todo Application</button></Link>
         <Link href="https://currymaker.vercel.app/"><button>Curry Maker</button></Link>
-        <Link href=""><button>Weather application</button></Link>
+        <Link href="https://expo.io/@kccodes/weatherapp"><button>Weather application(mobile)</button></Link>
         </p>
       </main>
     </div>
