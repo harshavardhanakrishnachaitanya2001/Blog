d06b997d1df272d9de4fc59565d88661f2d31e8e
Harshavardhan <187y1a0575@gmail.com>
2020-08-19 18:39:33 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          G.H.Krishna Chaitanya
+        </h1>
+
+        <p className={styles.description}>
+        Tasks done:<br />
+        <Link href="https://todoapplication-nu.vercel.app/"><button>Todo Application</button></Link>
+        <Link href="https://currymaker.vercel.app/"><button>Curry Maker</button></Link>
+        <Link href=""><button>Weather application</button></Link>
+        </p>
+      </main>
+    </div>
+  )
+}
