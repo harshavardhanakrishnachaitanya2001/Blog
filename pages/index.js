2f9e41b531cc9e61256731403dc279bb2b8a1782
Harshavardhan <187y1a0575@gmail.com>
2020-10-02 13:07:15 +0530

    added youtube link

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         text-align:center;
         font-size:20px;
       }
-      .profile{
+      img{
         border-radius:50%;
         height:120px;
         margin-left:5px;
@@ -36,7 +36,7 @@ export default function Home() {
 
         <h1 className={styles.title}>
           G.H.Krishna Chaitanya
-          <img className="profile" src="https://avatars3.githubusercontent.com/u/68343068?s=460&u=df8a95d0d4979f2ddc67bb8a809c1f88a55470fc&v=4"/>
+          <img src="https://avatars3.githubusercontent.com/u/68343068?s=460&u=df8a95d0d4979f2ddc67bb8a809c1f88a55470fc&v=4"/>
         </h1>
 
         <p className={styles.description}>
@@ -53,8 +53,8 @@ export default function Home() {
           I am G.H.Krishna Chaitanya, pursuing my B.tech IIIrd year in Marri Laxman Reddy instute of technology and management. 
           I am an enthusiastic front-end web developer, working primarily with Next.js along with CSS, Bootstrap, React-native, context, 
           git and React. I also enjoy working with java, C and python.<br />
-          I also have a Youtube channel where I explain all the concepts I know.
-          <a href='https://www.youtube.com/channel/UCZv-HrgSN0qc92gEZqDC3sA' target='_blank'> K.C. codes</a>
+          I also have a Youtube channel where I explain all the concepts I know.<br />
+          <Link href='https://www.youtube.com/channel/UCZv-HrgSN0qc92gEZqDC3sA'><a target='_blank'> K.C. codes</a></Link>
         </p>
     </div>
   )
