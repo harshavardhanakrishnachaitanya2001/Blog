5ad3d36228fd99f7794db850a0c0b9ec343ada24
Harshavardhan <187y1a0575@gmail.com>
2020-10-03 13:45:01 +0530

    Added a new application to the personal website

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,7 @@ export default function Home() {
         <Link href="/scoreBoard"><button>Score board</button></Link>
         <Link href="/weatherapp"><button>Weather application</button></Link>
         <Link href="/Anime"><button>Anime</button></Link>
+        <Link href="/svelteTodo"><button>Todo application: Mark II</button></Link>
         </p>
         <h1 className="aboutme">About me</h1>
         <p className="about">
---
+++ pages/svelteTodo.js
@@ -0,0 +1,39 @@
+import Link from 'next/link'
+import Head from 'next/head'
+const todoApp = () => {
+    return (
+        <div>
+        <style>
+            {`
+                h1{
+                    margin: 0;
+                     font-size: 50px;
+                     text-align:center;
+                }
+                p{
+                    text-align:center;
+                    font-size:25px;
+                }
+                button{
+                    margin-left:10px;
+                    font-size:20px;
+                    cursor:pointer;
+                }
+            `}
+        </style>
+        <Head>
+            <title>
+                Todo application: Mark:II
+            </title>
+        </Head>
+        <h1>Todo Applicaiton: Mark II</h1>
+        <p>
+        This todo application is built using sveltejs, a js framework. This website has a way to clear the entair list
+        (an 'empty list' button).Unlike the last one, insted of buttons beside every task, check boxes are used so that it looks more like a todo list.<br />
+        <Link href="https://todo-svelte-six.vercel.app/"><button>Todo application: Mark II</button></Link>
+        <Link href="/"><button>Home</button></Link>
+        </p>
+        </div>
+    )
+}
+export default todoApp
